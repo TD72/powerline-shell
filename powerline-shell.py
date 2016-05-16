3da5b1cc2a12e5475aca75fb8ccb197f275e736d
td72 <teledx72@gmail.com>
2016-05-16 16:50:13 +0900

    change default theme solarized

--- powerline-shell.py
+++ powerline-shell.py
@@ -18,11 +18,11 @@ class Powerline:
         'compatible': {
             'lock': 'RO',
             'network': 'SSH',
-            'separator': u'\uE0B0',
-            'separator_thin': u'\uE0B2'
+            'separator': u'\u25B6',
+            'separator_thin': u'\u276F'
         },
         'patched': {
-            'lock': '',
+            'lock': u'\uE0A2',
             'network': u'\uE0A2',
             'separator': u'\uE0B0',
             'separator_thin': u'\uE0B1'
@@ -211,6 +211,43 @@ class Color(DefaultColor):
     pass
 
 
+class Color(DefaultColor):
+    USERNAME_FG = 15
+    USERNAME_BG = 4
+    USERNAME_ROOT_BG = 1
+
+    HOSTNAME_FG = 15
+    HOSTNAME_BG = 10
+
+    HOME_SPECIAL_DISPLAY = False
+    PATH_FG = 7
+    PATH_BG = 10
+    CWD_FG = 15
+    SEPARATOR_FG = 14
+
+    READONLY_BG = 1
+    READONLY_FG = 7
+
+    REPO_CLEAN_FG = 14
+    REPO_CLEAN_BG = 0
+    REPO_DIRTY_FG = 3
+    REPO_DIRTY_BG = 0
+
+    JOBS_FG = 4
+    JOBS_BG = 8
+
+    CMD_PASSED_FG = 15
+    CMD_PASSED_BG = 2
+    CMD_FAILED_FG = 15
+    CMD_FAILED_BG = 1
+
+    SVN_CHANGES_FG = REPO_DIRTY_FG
+    SVN_CHANGES_BG = REPO_DIRTY_BG
+
+    VIRTUAL_ENV_BG = 15
+    VIRTUAL_ENV_FG = 2
+
+
 import os
 
 def add_virtual_env_segment():
@@ -396,7 +433,7 @@ GIT_SYMBOLS = {
     'behind': u'\u2B07',
     'staged': u'\u2714',
     'notstaged': u'\u270E',
-    'untracked': u'\u261E',
+    'untracked': u'\u2753',
     'conflicted': u'\u273C'
 }
 
